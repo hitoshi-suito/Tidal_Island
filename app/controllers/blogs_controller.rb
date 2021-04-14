702758e4f552c048f973f3e865c047ba23993c0d
hitoshi-suito <htshts153@gmail.com>
2021-04-14 22:59:24 +0900

    createアクション作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,12 +9,11 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-      if @blog.save
-        redirect_to blogs_path, notice: 'ブログを作成しました'
-      else
-        render :new
-      end
-
+    if @blog.save
+      redirect_to blogs_path, notice: 'ブログを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
