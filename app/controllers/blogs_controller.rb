36299dcc99417f111774fb9b0f7f82e9db733208
hitoshi-suito <htshts153@gmail.com>
2021-04-15 10:47:07 +0900

    editアクション作成,パーシャル化,ブログコントローラのリファクタリング

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
   end
@@ -17,11 +18,9 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: 'ブログを更新しました'
     else
@@ -30,11 +29,9 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def destroy
-    @blog = Blog.find(params[:id])
     if @blog.destroy
       redirect_to blogs_path, notice: 'ブログを削除しました'
     else
@@ -45,5 +42,9 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:title, :content, :image)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
