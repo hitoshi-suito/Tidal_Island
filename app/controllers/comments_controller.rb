65f684022728b3a104f43dff4826f2ab7edc1dcd
hitoshi-suito <htshts153@gmail.com>
2021-04-15 23:44:43 +0900

    コメント編集機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,35 @@
 class CommentsController < ApplicationController
+  before_action :set_blog, only: [:create, :edit, :update]
+  
   def create
-    @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.build(comment_params)
     @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
-        format.js{ render :index }
+        format.js { render :index }
       else
-        format.html{ redirect_to blog_path(@blog.id), notice: '投稿できませんでした' }
+        format.html { redirect_to blog_path(@blog.id), notice: '投稿できませんでした' }
+      end
+    end
+  end
+
+  def edit
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが更新されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集ができませんでした'
+        format.js { render :edit_error}
       end
     end
   end
@@ -15,5 +37,9 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:blog_id, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
   end
 end
