219a988fed37bcaf1fe0bc9fffea601525399d49
hitoshi-suito <htshts153@gmail.com>
2021-04-17 00:12:45 +0900

    tidesコントローラ、indexアクション追加

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -1,2 +1,9 @@
 class Tide < ApplicationRecord
+  def self.insert_column
+    tide = Tide.create(low_start_at: '23:00', low_end_at: '23:30', current_time: Time.now)
+    binding.pry
+    if tide.low_start_at > current_time
+      redirect_to blogs_path
+    end
+  end
 end
