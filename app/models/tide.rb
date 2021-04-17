bafa2b2f1ee7376991b30a5930102875bbd39d9c
hitoshi-suito <htshts153@gmail.com>
2021-04-17 12:43:09 +0900

    Tideモデルに６回分のログイン可能時刻記載

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -1,5 +1,10 @@
 class Tide < ApplicationRecord
   def self.insert_column
-    tide = Tide.create(low_start_at: '18:00', low_end_at: '23:30', current_time: Time.now)
+    tide = Tide.create(low_start_at: '0:30', low_end_at: '1:00', current_time: Time.now)
+    tide = Tide.create(low_start_at: '4:00', low_end_at: '4:30', current_time: Time.now)
+    tide = Tide.create(low_start_at: '7:00', low_end_at: '7:30', current_time: Time.now)
+    tide = Tide.create(low_start_at: '12:00', low_end_at: '12:30', current_time: Time.now)
+    tide = Tide.create(low_start_at: '17:00', low_end_at: '17:30', current_time: Time.now)
+    tide = Tide.create(low_start_at: '19:00', low_end_at: '19:30', current_time: Time.now)
   end
 end
