0e4a24309ac2dadae701293ab5e8258de0336ec1
hitoshi-suito <htshts153@gmail.com>
2021-04-20 23:17:50 +0900

    blog投稿のrspec記載

--- spec/factories/tides.rb
+++ spec/factories/tides.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :tide do
-    low_start_at { "2021-04-15 15:00:00" }
-    low_end_at { "2021-04-15 15:00:00" }
-    current_time { "2021-04-15 15:00:00" }
+    low_start_hour { 10 }
+    low_start_min { 0 }
+    low_end_hour { 10 }
+    low_end_min { 30 }
+    year { 2021}
+    month { 4 }
+    day { 10 }
   end
 end
--- spec/system/blog_spec.rb
+++ spec/system/blog_spec.rb
@@ -3,22 +3,34 @@ RSpec.describe 'ブログ投稿機能', type: :system do
   describe '新規作成機能' do
     context 'ブログを新規作成した場合' do
       it '作成したブログが表示される' do
-        blog = FactoryBot.create(:blog)
-        visit blogs_path
-        expect(page).to have_content 'blog'
+        user = FactoryBot.create(:user)
+        tide = FactoryBot.create(:tide)
+        visit new_user_session_path
+        fill_in 'user_email', with: 'hamada@example.com'
+        fill_in 'user_password', with: 'hamada'
+        click_on 'login_submit'
+        click_on 'create_new_blog'
+        fill_in 'blog_title', with: 'test'
+        fill_in 'blog_content', with: 'content'
+        click_on 'create_blog'
+        expect(page).to have_content 'test'
+        expect(page).to have_content 'content'
       end
     end
-  end
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのブログ一覧が表示される' do
+    context '画像を投稿した場合' do
+      it 'ブログ一覧に画像が表示される' do
+        user = FactoryBot.create(:user)
+        tide = FactoryBot.create(:tide)
+        visit new_user_session_path
+        fill_in 'user_email', with: 'hamada@example.com'
+        fill_in 'user_password', with: 'hamada'
+        click_on 'login_submit'
+        click_on 'create_new_blog'
+        fill_in 'blog_title', with: 'test'
+        fill_in 'blog_content', with: 'content'
+        attach_file 'image', "#{Rails.root}/app/assets/images/keep/test.jpg"
+        click_on 'create_blog'
       end
     end
-  end
-  describe '詳細表示機能' do
-     context '任意のブログ詳細画面に遷移した場合' do
-       it '該当ブログの内容が表示される' do
-       end
-     end
   end
 end
