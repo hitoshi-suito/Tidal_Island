ca6af1f690bfd15878d2325815f88565ef98e3e8
hitoshi-suito <htshts153@gmail.com>
2021-04-23 23:37:18 +0900

    commentモデルのrspec記載

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,5 +8,21 @@ RSpec.describe Comment, type: :model do
         expect(comment).not_to be_valid
       end
     end
+    context 'コメントのコンテントが入力されている場合' do
+      it 'バリデーションが通る' do
+        user = FactoryBot.create(:normal_user)
+        tide = FactoryBot.create(:tide)
+        blog = Blog.new(content: 'blog')
+        # binding.pry
+        blog.user_id = user.id
+        blog.tide_id = tide.id
+        blog.save
+        comment = Comment.new(content: 'comment')
+        comment.user_id = user.id
+        comment.blog_id = blog.id
+        # binding.pry
+        expect(comment).to be_valid
+      end
+    end
   end
 end
